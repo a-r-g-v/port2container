5ee4e652444696143d9701e91bbde18e0b58ad14
greyia <info@thun.ws>
2014-11-27 11:38:22 +0900

    WIP Docker

--- port2container.go
+++ port2container.go
@@ -1,18 +1,23 @@
 package main
+
 import(
     "fmt"
     "iptables"
     "env"
+    "docker"
 )
 
-func main()  {
 
+func main()  {
     // Load Env Value
     _, err := env.Load()    
     if err != nil {
         fmt.Print(err)
         return
     }
+
+    // Check Docker
+    fmt.Println(docker.Path)
     
     if env.DEBUG {
     output,err := iptables.Exec("-L")
--- src/docker/docker.go
+++ src/docker/docker.go
@@ -1,3 +1,24 @@
 package docker
 
+import(
+    "os/exec"
+    "os"
+    "fmt"
+    "env"
+)
+var Path string
+var err error
 
+func init() {
+   Path, err = exec.LookPath("docker")
+   if err != nil {
+        fmt.Printf("Error pkg/docker : %s",err);
+        os.Exit(-1);
+   }
+}
+
+func Exec() {
+    if env.DEBUG == true {
+        fmt.Printf("Docker path is %s\n",Path) 
+    }
+}
